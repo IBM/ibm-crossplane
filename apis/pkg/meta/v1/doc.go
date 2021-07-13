e00377d9a5ba5d86084af57e38ea477a6864e507
ppyt-pl <84075708+ppyt-pl@users.noreply.github.com>
2021-07-13 11:23:25 +0200

    Newoperator (#24)
    * ownerReference fix  (#20)
    
    * fix
    
    Signed-off-by: aobrzut <artur.obrzut@pl.ibm.com>
    
    * Update reconciler.go
    
    * Update RELEASE_VERSION
    
    * Update RELEASE_VERSION
    
    * update crds to have namespace scope and new api group
    
    * Update README.md
    
    * Update README.md
    
    * Create temp.file
    
    * Create 123.temp
    
    * Delete temp.file
    
    * Delete 123.temp
    
    * Create pliczek
    
    * Delete pliczek
    
    * Create plikor
    
    * Delete plikor
    
    * change api group and scope
    
    * fix typo
    
    * generated yamls
    
    * add api groups to register files
    
    Co-authored-by: Artur Obrzut <54812713+arturobrzut@users.noreply.github.com>

--- apis/pkg/meta/v1/doc.go
+++ apis/pkg/meta/v1/doc.go
@@ -16,6 +16,6 @@ limitations under the License.
 
 // Package v1 contains meta types used to configure Crossplane packages.
 // +kubebuilder:object:generate=true
-// +groupName=meta.pkg.crossplane.io
+// +groupName=meta.pkg.ibm.crossplane.io
 // +versionName=v1
 package v1
