454b5e5f87eda7ff653ee3fe604a541abdb7cdc6
Pawel Kopel <Pawel.Kopel@ibm.com>
2021-07-21 12:11:52 +0200

    Add 1.1 changes to internal/client

--- internal/client/clientset/versioned/fake/register.go
+++ internal/client/clientset/versioned/fake/register.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The Crossplane Authors.
+Copyright 2021 The Crossplane Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -32,7 +32,7 @@ import (
 
 var scheme = runtime.NewScheme()
 var codecs = serializer.NewCodecFactory(scheme)
-var parameterCodec = runtime.NewParameterCodec(scheme)
+
 var localSchemeBuilder = runtime.SchemeBuilder{
 	apiextensionsv1.AddToScheme,
 	pkgv1alpha1.AddToScheme,
