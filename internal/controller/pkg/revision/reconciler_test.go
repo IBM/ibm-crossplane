dbeb1cb1dc4c6d9d9407e0b44ea1fb8a190edb35
Jan-Olszowka <111862576+Jan-Olszowka@users.noreply.github.com>
2023-01-12 14:10:54 +0100

    Update copyright-2023 (#91)

--- internal/controller/pkg/revision/reconciler_test.go
+++ internal/controller/pkg/revision/reconciler_test.go
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */ //
-// Copyright 2021 IBM Corporation
+// Copyright 2023 IBM Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
