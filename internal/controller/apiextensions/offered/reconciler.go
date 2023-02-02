cfd1e1a92e5c5d87bb2d18652a4dd4d1be5dad70
Jan Olszowka <jan.olszowka@ibm.com>
2023-02-02 08:57:13 +0100

    updated copyrights

--- internal/controller/apiextensions/offered/reconciler.go
+++ internal/controller/apiextensions/offered/reconciler.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 //
-// Copyright 2021 IBM Corporation
+// Copyright 2023 IBM Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
